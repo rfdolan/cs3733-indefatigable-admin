7fa415382de8422c6321fb4c061e5c30058d8a25
René Borner <mail@reneborner.de>
2019-12-05 15:58:58 -0500

    added reload

--- src/RemoteSitePanel.js
+++ src/RemoteSitePanel.js
@@ -5,7 +5,7 @@ import RemoteSite from './RemoteSite.js'
 const get_url = base_url + "getAllRemoteSites"
 
 class RemoteSitePanel extends React.Component {
-    _isMounted = false;
+    _isMounted = false
 
     state = {
         sites: [],
@@ -15,24 +15,24 @@ class RemoteSitePanel extends React.Component {
     // These two functions make us promise not to update the state if the component
     // is not mounted.
     componentDidMount() {
-        this._isMounted = true;
-        this.getSites();
+        this._isMounted = true
+        this.getSites()
     }
 
     componentWillUnmount() {
-        this._isMounted = false;
+        this._isMounted = false
     }
 
     getSites = () => {
         let xhr = new XMLHttpRequest()
         xhr.open("GET", get_url, true)
-        console.log("Sending");
+        console.log("Sending")
         xhr.send()
         xhr.onloadend = () => {
             if (xhr.readyState === XMLHttpRequest.DONE) {
-                this.processGetSitesResponse( xhr.responseText)
+                this.processGetSitesResponse(xhr.responseText)
             } else {
-                this.processGetSitesResponse( "N/A")
+                this.processGetSitesResponse("N/A")
             }
         }
     }
@@ -46,41 +46,44 @@ class RemoteSitePanel extends React.Component {
             this.setState({
                 sites: js["urls"]
             })
-            this.renderSites();
+            this.renderSites()
         } else {
             console.log("Couldn't get")
         }
     }
 
     renderSites = () => {
-        let sites = [];
-        for(let i=0; i<this.state.sites.length; i++) {
-            let currSite = this.state.sites[i];
-            sites.push(<li key={currSite.url} style={{listStyleType: "none", padding: "5px", float: "left"}}><RemoteSite url={currSite}  /></li> )
+        let sites = []
+        for (let i = 0; i < this.state.sites.length; i++) {
+            let currSite = this.state.sites[i]
+            sites.push(<li key={currSite.url} style={{listStyleType: "none"}}>
+                <RemoteSite url={currSite}/></li>)
         }
-        return sites;
+        return sites
     }
     handleChange = (e) => {
         this.setState({
             [e.target.name]: e.target.value
         })
-        console.log(this.state.siteToRegister);
+        console.log(this.state.siteToRegister)
     }
 
     render() {
         //console.log(this.state);
         return (
-            <div >
-                Enter new remote site url: 
-                <input type="text" style={{margin:"5px"}} />
-                <button type="submit" name="siteToRegister" value={this.state.siteToRegister} onChange={e => this.handleChange(e)}style={{margin:"5px"}} >Register</button>
-                <br />
+            <div>
+                Enter new remote site url:
+                <input type="text" style={{margin: "5px"}}/>
+                <button type="submit" name="siteToRegister" value={this.state.siteToRegister}
+                        onChange={e => this.handleChange(e)} style={{margin: "5px"}}>Register
+                </button>
+                <br/>
                 {this.renderSites()}
-                <br />
+                <br/>
             </div>
         )
     }
 
 }
 
-export default RemoteSitePanel;
+export default RemoteSitePanel
--- src/VideoPanel.js
+++ src/VideoPanel.js
@@ -5,13 +5,14 @@ import './style/VideoPanel.css'
 import base_url from './api/api.js'
 
 const get_all_videos_url = base_url + "getAllSegments"
+
 /*
 const sampleData = [{title: "Vid Title", transcript: "It is illogical", url: "https://cs3733-indefatigable.s3.us-east-2.amazonaws.com/media/Kirk-ItIsIllogical.ogg",
 character: "Kirk", isRemote: false, isRemoteAvailable: true}, {title: "Vid2 Title", transcript: "Arg", url: "https://cs3733-indefatigable.s3.us-east-2.amazonaws.com/media/fisher-agh.ogg",
 character: "Fisher", isRemote: true, isRemoteAvailable: true}];
 */
 class VideoPanel extends React.Component {
-    _isMounted = false;
+    _isMounted = false
 
     state = {
         videos: [],
@@ -20,36 +21,48 @@ class VideoPanel extends React.Component {
     // These two functions make us promise not to update the state if the component
     // is not mounted.
     componentDidMount() {
-        this._isMounted = true;
-        this.getAllVideos();
+        this._isMounted = true
+        this.getAllVideos()
     }
 
     componentWillUnmount() {
-        this._isMounted = false;
+        this._isMounted = false
     }
 
-    
+
     getAllVideos = () => {
         axios.get(get_all_videos_url)
-        .then((res) => {this.setState( { videos: res.data.list })})
-        .then(() => {if(this._isMounted) {this.renderVideos()}});
+            .then((res) => {
+                this.setState({videos: res.data.list})
+            })
+            .then(() => {
+                if (this._isMounted) {
+                    this.renderVideos()
+                }
+            })
     }
-    
+
 
     renderVideos = () => {
         // Array of JSX videos we want to render.
-        let vids = [];
-        for(let i=0; i<this.state.videos.length; i++ ) {
-            let currVid = this.state.videos[i];
+        let vids = []
+        for (let i = 0; i < this.state.videos.length; i++) {
+            let currVid = this.state.videos[i]
             // If we are not filtering by local only OR if we are and this is a local video, add it to the array.
-            if( !currVid.isRemote){
+            if (!currVid.isRemote) {
                 //console.log(currVid);
-                vids.push(<li key={currVid.vuid} style={{listStyleType: "none", padding: "5px", float: "left"}}><Video title={currVid.title} transcript={currVid.transcript} url={currVid.url} 
-                    character={currVid.character} isRemote={currVid.isRemote} isRemotelyAvailable={currVid.remoteAvailability} id={currVid.vuid} markingHandler={this.getAllVideos}></Video></li>);
+                vids.push(
+                    <li key={currVid.vuid} style={{listStyleType: "none", padding: "5px"}}>
+                        <Video title={currVid.title} transcript={currVid.transcript} url={currVid.url}
+                               character={currVid.character} isRemote={currVid.isRemote}
+                               isRemotelyAvailable={currVid.remoteAvailability} id={currVid.vuid}
+                               markingHandler={this.getAllVideos}
+                        />
+                    </li>)
             }
         }
         // Return our JSX tags to render.
-        return vids;
+        return vids
 
     }
 
@@ -57,12 +70,12 @@ class VideoPanel extends React.Component {
         return (
             <div>
                 <div>
-                    <br />
-                    <br />
+                    <br/>
+                    <br/>
                     <form>
-                        <label style={{display:"inline-block"}}>
+                        <label style={{display: "inline-block"}}>
                             Search:
-                            <input type="text" style={{margin: "5px"}} />
+                            <input type="text" style={{margin: "5px"}}/>
                             <select style={{margin: "5px"}}>
                                 <option value="any">Any character</option>
                                 <option value="Kirk">Kirk</option>
@@ -74,13 +87,14 @@ class VideoPanel extends React.Component {
                         </label>
                     </form>
                 </div>
-                <br />
-                <button type="button" onClick={this.uploadNewSegment}>Upload new video</button><br />
-                <br />
+                <br/>
+                <button type="button" onClick={this.uploadNewSegment}>Upload new video</button>
+                <br/>
+                <br/>
                 {this.renderVideos()}
             </div>
-        );
+        )
     }
 }
 
-export default VideoPanel;
+export default VideoPanel
